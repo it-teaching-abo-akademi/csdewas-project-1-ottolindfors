54fd65a3e9667dd02cf375cc689c9e1a166d0110
otto <otto.lindfors@abo.fi>
2019-11-17 19:41:16 +0200

    Some html and js

---
+++ exercise-1/js/scripts.js
@@ -0,0 +1,9 @@
+// This will run once the page has finished loading
+
+// The function is not executed by itself because someone or something has to execute it
+function functionName() {
+    element.event = FunctionName;
+    element.event = FunctionName;
+}
+// Once all the content of the htm is loaded the function will be called and executed
+window.onload = functionName; // global code
