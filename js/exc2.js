a354959b828f24f5fbe53053dd2379becc45ef8e
otto <otto.lindfors@abo.fi>
2019-11-25 17:47:11 +0200

    Barcode added

--- js/exc2.js
+++ js/exc2.js
@@ -1,9 +1,12 @@
+// Event listener for making input field gray on focus
 document.getElementById("barcodeInput").addEventListener("focus", function(){
     document.getElementById("barcodeInput").style.backgroundColor = "lightgray";
 });
+// Event listener for making input field pink on blur (off focus)
 document.getElementById("barcodeInput").addEventListener("blur", function(){
-    document.getElementById("barcodeInput").style.backgroundColor = "pink";
+    document.getElementById("barcodeInput").style.backgroundColor = "white";
 });
+
 /* Run when DOM has loaded (use "load" to wait till all content has loaded)
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("barcodeInput").addEventListener("focus", function(){
@@ -14,3 +17,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 });
 */
+
+// Event listener on the 'Decode' button
+document.getElementById("buttonDecode").addEventListener("click", function () {
+    const text = document.getElementById("barcodeInput").value;
+    try {
+        JsBarcode("#barcode", text);
+        document.getElementById("errorMessage").innerHTML = "";
+        document.getElementById("barcodeInput").style.backgroundColor = "palegreen";
+    }
+    catch(err) {
+        document.getElementById("errorMessage").innerHTML = err.message;
+        document.getElementById("barcodeInput").style.backgroundColor = "pink";
+    }
+});
