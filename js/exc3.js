4fe7a268c94743889d7420b53de41874b2cbe5d3
otto <otto.lindfors@abo.fi>
2019-11-28 23:34:26 +0200

    updates to exercise 3

--- js/exc3.js
+++ js/exc3.js
@@ -6,14 +6,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // Add event listener for showRoute button
 document.getElementById("showRoute").addEventListener("click", function(){
-    showRoute();
+    showRouteOnMap();
 });
 
 let map;
 const inilat = 60.4545;
 const inilon = 22.2648;
 const routesUrl = "https://data.foli.fi/gtfs/routes";
+const tripsUrlBase = "https://data.foli.fi/gtfs/v0/trips/route/";
 const shepesUrlBase = "https://data.foli.fi/gtfs/shapes";
+let routeData = [];  // route_short_name, route_long_name, route_id
 
 function drawMap() {
     let tile = new ol.layer.Tile({
@@ -45,13 +47,16 @@ function fetchRoutes() {
             }
         })
         .then(function (data) {
-            let routes = [];
             data.map(
-                functionInput => routes.push(
-                    {route_short_name: functionInput.route_short_name, route_long_name: functionInput.route_long_name }
+                functionInput => routeData.push(
+                    {
+                        route_short_name: functionInput.route_short_name,
+                        route_long_name: functionInput.route_long_name,
+                        route_id: functionInput.route_id
+                    }
                 )
             );
-            updateBusList(routes);
+            updateBusList(routeData);
         })
         .catch(function (err) {
             alert("Something went wrong :-S\n(Developer, see console for more information.)")
@@ -63,18 +68,32 @@ function updateBusList(arr) {
     arr.sort();
     for (i=0; i<arr.length; i++) {
         var option = document.createElement("option");
-        option.innerHTML = arr[i].route_short_name;
+        option.value = arr[i].route_id;
+        option.innerHTML = arr[i].route_short_name + " - " + arr[i].route_long_name;
         document.getElementById("busList").appendChild(option);
         // option.innerHTML = arr[i].route_short_name + " " + arr[i].route_long_name;
     }
 }
 
-function showRoute() {
+function showRouteOnMap() {
     /*
     Fetch the shape of the route and draw it on the map
      */
     // TODO: Get url from selected route
+
+    // Acquire route_id
+    let route_id = getSelectedRouteId();
+    // Acquire list of trips belonging to the route_id from trips.txt
+    let shape_ids = getShapeIDsForRoute(route_id);
+    // Pick the most common shape_id from list of trips
+    console.log(shape_ids);
+    console.log("shape_ids.length: " + shape_ids.length);
+    let shape_id = calculateMostCommonShape(shape_ids);
+    console.log("shape_id: " + shape_id);
+    // Acquire coordinate listing by shape_id
+
     const url = 'https://data.foli.fi/gtfs/v0/20191114-135003/shapes/0_201';
+
     fetch(url)
         .then(function (response) {
             // The JSON data will arrive here
@@ -103,9 +122,48 @@ function getRouteShapeUrl() {
 
 }
 
-function getSelectedBus() {
+function getSelectedRouteId() {
+    /*
+    Returns the selected route_id from the drop down list.
+    The route_id is stored in the value
+     */
     let busList = document.getElementById("busList");
-    return busList.options[busList.selectedIndex].text;  // type=string
+    return busList.options[busList.selectedIndex].value;  // type=string
+}
+
+function getShapeIDsForRoute(route_id) {
+    // Acquire list of trips belonging to the route_id from trips.txt and return shapes of all trips
+    // TODO: consider reusing code
+
+    let url = tripsUrlBase + route_id;
+    let shape_ids = [];
+
+    fetch(url)
+        .then(function (response) {
+            if (response.status >= 200 && response.status < 400) {
+                return response.json();
+            }
+            else {
+                alert("Could not fetch trip :-S\n\n(Response status: " + response.status + ")");
+                console.log(response.status);
+            }
+        })
+        .then(function (data) {
+            data.map(fnctInput => shape_ids.push(fnctInput.shape_id));
+        })
+        .catch(function (err) {
+            console.log("Something went wrong :-S " + err);
+            alert("Something went wrong :-S\n(Developer, see console for more information.)")
+        });
+    return shape_ids;
+}
+
+function calculateMostCommonShape(shape_ids_array) {
+    // Calculate most common shape_id in shape_ids_array
+    // Currently only picking a random shape_id but will be improved later
+    rand_idx = Math.floor(Math.random() * shape_ids_array.length);
+    console.log(shape_ids_array.length + " : " + rand_idx);
+    return shape_ids_array[rand_idx];
 }
 
 function drawBusLine(coordinates) {
